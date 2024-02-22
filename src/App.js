d886a997be4aa1abd993c7845ad6b830f214ac52
nishantsahu <nishant.sahu106@gmail.com>
2024-02-22 16:17:22 +0530

    added routes and homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import NavBar from "./NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
